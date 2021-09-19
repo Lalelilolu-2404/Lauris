a821006ac15d7bc3f329a7eba6b7f58ab3fffb7a
Death <85760563+Lalelilolu-2404@users.noreply.github.com>
2021-09-19 17:09:48 +0200

    Update samu.js

--- samu.js
+++ samu.js
@@ -2166,6 +2166,9 @@ stckr = `╭⸻⃞✫꯭𝙈꯭𝙀꯭𝙉꯭𝙐꯭✫⃞⸻╮
 ╭─────────────
 │ *Amm*
 │ *Ban*
+│ *Colita*
+│ *Comida de Anna*
+│ *Gansito*
 │ *Funao*
 │ *Love*
 │ *Ya se durmieron*
@@ -2294,11 +2297,11 @@ break
 
 case 'forme':
 const morestickx = ["Abasho", "Lean las reglas", "Nel mijo", "Se la coge", "Soy inevitable", "Le mete el brazo", 
-		  "Fbi", "X el chikito", "Xd", "Comida de Anna", "Nel pastel", "Despreciox"]		
+		  "Fbi", "X el chikito", "Xd", "Comida de Anna", "Nel pastel", "Despreciox", "Se relaja", "Colitax", "Gansito"]		
 const onlyme = ["69", "Ahohsi", "Alto", "A mira nomás", "A2", "Abrazo", "Ah ok", "Ahh", "Amistad", "Anna", "Amm", "Anni", "Así me gusta", "Asustado", "Ay caramba", "Ay me asusté", 
 		"Ban", "Báñate", "Besos", "Bien", "Bragas", "Buen culo", "Buenas noches", "Bye", "C rasca", "Cállate", "Cc", "Chelitas", "Chongo", "Colita", "Colitaxxx", 
 		"Contesta", "Contra el muro", "Ctm", "Cunni", "Dame", "Delito", "Depre", "Doncon", "Duren", "En fin", "Encuérate", "Eres bonita", "F el grupo", "F en el chat", 
-		"F", "Facha", "Fallesí", "Faptality", "FBI", "Flap", "Funao", "Gansito", "Gatita", "Gomitas", "Háblame bonito", "Hola", "Hack", "Haha no", "Hahaha", "Helado", "Hentai", 
+		"F", "Facha", "Fallesí", "Faptality", "FBI", "Flap", "Funao", "Gatita", "Gomitas", "Háblame bonito", "Hola", "Hack", "Haha no", "Hahaha", "Helado", "Hentai", 
 		"I wanna fuck you", "Ily", "Im dead", "Inflingir", "Jálame", "Jiji", "Jutsu", "Kuaker", "La ahorca", "La ata", "La nalguea", "Las bragitas", "Lau", "Le muerde", "Lit", 
 		"Llegó papi", "Lolxd", "Lo mira feo", "Love", "Ly", "Me das admin", "Me dormí", "Me encanta", "Me prometiste", "Me vengo", "Meyou", "Mimitos", "Muy bien", "Nani", "Nel", 
 		"Nezuko chan", "No antojes", "No me interesa", "No mires", "No puede ser", "No sé", "No te excites", "No te rías xd", "Oh yeah mami", "Onichan", "Ouioui", "Pack", "Packsito pls", "Paja", "Paja2", "Pansito", "Pantsu", "Patito", 
@@ -8353,15 +8356,20 @@ if (isAllaud) {
 		}
 	} 
 	if (body.includes(`Comida de Anna`) && body.length == 14){
-		randc = Math.floor(Math.random() * 5)
+		randc = Math.floor(Math.random() * 7)
 		none2 = fs.readFileSync(`./src/stickers2/Comida de Anna${randc}.webp`)
 		samu330.sendMessage(from, none2, sticker, {quoted: sam})
+	} 
+	if (body.includes(`Gansito`) && body.length == 7){
+		randc = Math.floor(Math.random() * 5)
+		none2 = fs.readFileSync(`./src/stickers2/Gansito${randc}.webp`)
+		samu330.sendMessage(from, none2, sticker)
 	} 
 }
 if (!isOwner && !isCherry){	
 if (!isAllaud) return
 const Fer =  ["Me das admin", "Ban", "Funao", "Love", "Ya se durmieron", "Te me calmas", "Ta fuerte", "Takeself", "Hahaha", "Hola", "Faptality", "F el grupo", "Ctm", "Shh", "Nani", 
-	      "Lolxd", "F", "Amm", "Pichula", "Si xd", "Haha no", "Paja2", "Perro", "Ufff"]		
+	      "Lolxd", "F", "Amm", "Pichula", "Si xd", "Haha no", "Paja2", "Perro", "Ufff", "Colita"]		
   if (!isBan){
 	for (let i = 0; i < Fer.length; i++){
 		if (body.includes(`${Fer[i]}`) && body.length == Fer[i].length){
@@ -8406,7 +8414,7 @@ if (!isAllaud) return
 const stickme = ["69", "Ahohsi", "Alto", "A mira nomás", "A2", "Abrazo", "Ah ok", "Ahh", "Amistad", "Anna", "Amm", "Anni", "Así me gusta", "Asustado", "Ay caramba", "Ay me asusté", 
 		"Ban", "Báñate", "Besos", "Bien", "Bragas", "Buen culo", "Buenas noches", "Bye", "C rasca", "Cállate", "Cc", "Chelitas", "Chongo", "Colita", "Colitaxxx", 
 		"Contesta", "Contra el muro", "Ctm", "Cunni", "Dame", "Delito", "Depre", "Doncon", "Duren", "En fin", "Encuérate", "Eres bonita", "F el grupo", "F en el chat", 
-		"F", "Facha", "Fallesí", "Faptality", "FBI", "Flap", "Funao", "Gansito", "Gatita", "Gomitas", "Háblame bonito", "Hack", "Haha no", "Hahaha", "Helado", "Hentai", "Hola", 
+		"F", "Facha", "Fallesí", "Faptality", "FBI", "Flap", "Funao", "Gatita", "Gomitas", "Háblame bonito", "Hack", "Haha no", "Hahaha", "Helado", "Hentai", "Hola", 
 		"I wanna fuck you", "Ily", "Im dead", "Inflingir", "Jálame", "Jiji", "Jutsu", "Kuaker", "La ahorca", "La ata", "La nalguea", "Las bragitas", "Lau", "Le muerde", 
 		"Lit", "Llegó papi", "Lolxd", "Lo mira feo", "Love", "Ly", "Me das admin", "Me dormí", "Me encanta", "Me prometiste", "Me vengo", "Meyou", "Mimitos", "Muy bien", 
 		"Nani", "Nel", "No antojes", "No me interesa", "No mires", "No puede ser", "No sé", "No te excites", "No te rías xd", "Oh yeah mami", "Onichan", "Ouioui", 
@@ -8426,8 +8434,8 @@ if (!isAllaud) return
 	}
 }
 //////////////	
-const morestick = ["Abasho", "Lean las reglas", "Nel mijo", "Se la coge", "Soy inevitable", "Le mete el brazo", 
-		  "Fbi", "X el chikito", "Nel pastel", "Desprecio1", "Desprecio2", "Desprecio3"]	
+const morestick = ["Abasho", "Lean las reglas", "Nel mijo", "Se la coge", "Soy inevitable", "Le mete el brazo", "Pum", "Semen", "No viste nada", 
+		   "Fbi", "X el chikito", "Nel pastel", "Desprecio1", "Desprecio2", "Desprecio3", "Se relaja", "Colitax"]		
 
 if (isLalelilolu){
 if (!isAllaud) return
@@ -8462,7 +8470,7 @@ if (body.includes(`Xd`) && body.length == 2){
 			//body: '',
 			//sourceUrl : 'https://chat.whatsapp.com/LKZOC09DoJb6lwS5UUZRlj', 
 			//sourceUrl : 'https://api.whatsapp.com/send?phone=12729998838',
-			sourceUrl : 'https://wa.me/12729998838', 
+			//sourceUrl : 'https://wa.me/12729998838', 
 			thumbnail : fs.readFileSync('./src/nsfw.jpg')}}}
 		)
 }
