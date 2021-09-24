917028ed3730ad056122b3b8ff27dc3d0e4efe9d
Death <85760563+Lalelilolu-2404@users.noreply.github.com>
2021-09-24 03:09:05 +0200

    Update samu.js

--- samu.js
+++ samu.js
@@ -1537,7 +1537,7 @@ reply(`${_0x4dab[8]}${test[_0x4dab[7]](_0x4dab[4])}${_0x4dab[9]}${orlist[_0x4dab
     
 //Zona de Comandos🛵
 switch (command) {
-/**		
+		
 case 'menu18':
 samu330.updatePresence(from, Presence.composing)
 uptime = process.uptime()
@@ -1547,35 +1547,33 @@ if (!isNsfw) return reply(mess.nsfw)
 
 const menuxxx = `_*NORPO... Y GIFS*_
 
-${bodyM} ${prefix}lesbian		 ${bodyM} ${prefix}pussyg 
-${bodyM} ${prefix}tetas			   ${bodyM} ${prefix}boobsg
-${bodyM} ${prefix}ass			   ${bodyM} ${prefix}analg
+${bodyM} ${prefix}lesbian		   ${bodyM} ${prefix}pussyg 
+${bodyM} ${prefix}tetas			      ${bodyM} ${prefix}boobsg
+${bodyM} ${prefix}ass			      ${bodyM} ${prefix}analg
 ${bodyM} ${prefix}pussy
 
 _*HENTAI :3*_
 
-${bodyM} ${prefix}xwaifu		     ${bodyM} ${prefix}xneko
-${bodyM} ${prefix}trap			     ${bodyM} ${prefix}blow
-${bodyM} ${prefix}hentai		     ${bodyM} ${prefix}ahegao
-${bodyM} ${prefix}xboobs		  ${bodyM} ${prefix}xass
+${bodyM} ${prefix}xwaifu		      ${bodyM} ${prefix}xneko
+${bodyM} ${prefix}trap			      ${bodyM} ${prefix}blow
+${bodyM} ${prefix}hentai		      ${bodyM} ${prefix}ahegao
+${bodyM} ${prefix}xboobs		   ${bodyM} ${prefix}xass
 ${bodyM} ${prefix}muslos		   ${bodyM} ${prefix}patas
-${bodyM} ${prefix}futa			    ${bodyM} ${prefix}sidebobs
-${bodyM} ${prefix}blowjob		  ${bodyM} ${prefix}armpits
+${bodyM} ${prefix}futa			      ${bodyM} ${prefix}sidebobs
+${bodyM} ${prefix}blowjob		   ${bodyM} ${prefix}armpits
 ${bodyM} ${prefix}femdom		${bodyM} ${prefix}pussyimg
-${bodyM} ${prefix}oppai			  ${bodyM} ${prefix}cumimg`
-
+${bodyM} ${prefix}oppai			   ${bodyM} ${prefix}cumimg`
 		
 bahmh = fs.readFileSync(`./src/+18.jpg`)
 samu330.sendMessage(from, {jpegThumbnail: bahmh}, MessageType.liveLocation,{ quoted: fnsfw})
 setTimeout(() => {
-samu330.sendMessage(from, ` *Hola : ${pushname}*\n\n${menuxxx}`, MessageType.text)
+reply(` *Hola : ${pushname}*\n\n${menuxxx}`
 }, 1000)	
 
-sendLoct(from, ` *Hola : ${pushname}*\n\n${menuxxx}`, `© Creator\n⛧⸸⁶Death⁹†حب♡ت`, {jpegThumbnail: fs.readFileSync('./src/+18.jpg')})	
 addFilter(from)
 addLevelingXp(sender, 20)
 break	
-**/
+/**
 case 'menu18':
 samu330.updatePresence(from, Presence.composing)
 if (!isRegister) return samu330.sendMessage(from, notreg, MessageType.text, { quoted: noreg})
@@ -1607,7 +1605,7 @@ addFilter(from)
 addLevelingXp(sender, 20)		
 samu330.sendMessage(from, samuPn, image, { quoted: fnsfw, caption: `${menuxxx}`, thumbnail: samuPn})              
 break
-		
+**/		
 case 'help':
 case 'menu':
 case 'comandos':
