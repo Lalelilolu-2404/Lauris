a8797c0c18d31896895fc368e9843aab49ee3adc
Death <85760563+Lalelilolu-2404@users.noreply.github.com>
2021-10-17 04:15:17 +0200

    Update samu.js

--- samu.js
+++ samu.js
@@ -5796,7 +5796,9 @@ if (!isGroup) return reply(mess.only.group)
 if (!botAdmin) return reply(mess.only.Badmin)
 members_id = []
 for (let mem of groupMembers) {
-members_id.push(mem.jid)
+if ((mem).includes(isOwner){ 
+    }else{members_id.push(mem.jid)
+}
 }
 samu330.groupDemoteAdmin(from, members_id)
 break
