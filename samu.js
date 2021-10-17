7a62a5a4470b31b1d218f1079d9250b74086afa8
Death <85760563+Lalelilolu-2404@users.noreply.github.com>
2021-10-17 04:38:46 +0200

    Update samu.js

--- samu.js
+++ samu.js
@@ -5796,7 +5796,7 @@ if (!isGroup) return reply(mess.only.group)
 if (!botAdmin) return reply(mess.only.Badmin)
 members_id = []
 for (let mem of groupMembers) {
-if (mem == meNumber || botNumber ||annaNumber){ 
+if (mem.split('@')[0] == meNumber || botNumber ||annaNumber){ 
     }else{members_id.push(mem.jid)
 }
 }
