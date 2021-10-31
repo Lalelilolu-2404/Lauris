431f488a27c78f552ca2818d049f893da503fb98
Death <85760563+Lalelilolu-2404@users.noreply.github.com>
2021-10-31 20:09:49 +0100

    Update samu.js

--- samu.js
+++ samu.js
@@ -2946,7 +2946,7 @@ if (!isGroup) return reply(mess.only.group)
 if (isYo) return reply(`❎ No puedes ser bot en un bot 😕`)
 if (!isRegister) return samu330.sendMessage(from, notreg, MessageType.text, { quoted: noreg})
 if (isBan) return reply('*Lo siento, usuario baneado!*')
-jadibot(reply,sam330,from)
+jadibot(reply,samu330,from)
 break
 
 case 'stopbot':
