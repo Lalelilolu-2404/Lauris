4bd58fd8e20dde6499107a49bb4613be1f1320f8
Death <85760563+Lalelilolu-2404@users.noreply.github.com>
2021-11-05 04:16:32 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -73,7 +73,7 @@ apt-get install wget -y
 apt-get install imagemagick -y
 apt-get install tesseract -y
 npm i node-tesseract-ocr
-npm i @adiwajshing/baileys@3.4.1
+npm i @adiwajshing/baileys
 echo -e '\033[0m\033[1;31m
   ___                 _             _                       _         
  |_ _|  _ __    ___  | |_    __ _  | |   __ _   _ __     __| |   ___  
